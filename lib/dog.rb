a793d69fb1b32088b8bd67c383a77ad75a019d9b
Lindsay Mecher <lindsaymecher@gmail.com>
2020-01-07 17:10:07 -0600

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,31 @@
+=begin
+
+class Dog
+    def name=(dog_name)
+      dog_name = @this_dogs_name
+    end
+  
+    def name
+      @this_dogs_name
+    end
+  end
+
+  lassie = Dog.new
+lassie.this_dogs_name = "Lassie"
+=end
+
+class Dog
+
+    def name=(dogs_name)
+      @this_dogs_name = dogs_name
+    end
+  
+    def name
+      @this_dogs_name
+    end
+  end
+  
+  lassie = Dog.new
+  lassie.name = "Lassie"
+  
+  puts lassie.name
